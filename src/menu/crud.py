39b11321c438ea40f4e868fafebe52c53ac3f6c2
Welison92 <welison123santos91@gmail.com>
2025-04-28 23:07:22 -0300

    Atualização dos arquivos crud.py e routers.py da pasta menu

---
+++ src/menu/crud.py
@@ -0,0 +1,501 @@
+# Imports do sistema
+from collections import Counter
+from pathlib import Path
+
+# Imports de terceiros
+from fastapi import File, UploadFile
+from sqlalchemy.orm import Session
+from sqlalchemy.sql import func
+
+# Imports locais
+from src.menu.models import ItemModel, PedidoItensModel, PedidoModel
+from src.menu.schemas import (
+    DetalhePedido, MenuItem, PedidoClienteInput,
+    PedidoClienteOutput, StatusPedido
+)
+
+BASE_DIR = Path(__file__).resolve().parent.parent.parent  # Raiz do projeto
+IMAGES_DIR = BASE_DIR / "static" / "images"  # Diretório das imagens
+
+
+def get_menu(
+        db: Session,
+        categoria: str = None
+):
+    """
+    Retorna o cardápio completo ou filtrado por categoria.
+
+    Args:
+        categoria (str): Categoria para filtrar os itens do cardápio.
+        db (Session): Sessão do banco de dados.
+    Returns:
+        list: Lista de itens do cardápio.
+    """
+    # Verifica se a categoria foi fornecida
+    if categoria:
+        # Filtra os itens do cardápio pela categoria
+        menu = db.query(ItemModel).filter(
+            ItemModel.categoria.ilike(f"%{categoria.lower()}%")
+        ).all()
+    else:
+        # Retorna todos os itens do cardápio
+        menu = db.query(ItemModel).all()
+
+    # Converte os itens do cardápio para o formato desejado
+    return [MenuItem(**item.__dict__) for item in menu]
+
+
+def get_item_by_id(
+        db: Session,
+        item_id: int
+):
+    """
+    Retorna um item do cardápio pelo ID.
+
+    Args:
+        item_id (int): ID do item.
+        db (Session): Sessão do banco de dados.
+    Returns:
+        MenuItem: Item do cardápio.
+    """
+    # Busca o item pelo ID no banco de dados
+    item = db.query(ItemModel).filter(ItemModel.id == item_id).first()
+
+    # Verifica se o item foi encontrado
+    if not item:
+        return None
+
+    # Converte o item para o formato desejado
+    return MenuItem(**item.__dict__)
+
+
+def get_all_orders(db: Session):
+    """
+    Retorna todos os pedidos realizados.
+
+    Returns:
+        list: Lista de pedidos.
+    """
+    # Busca todos os pedidos no banco de dados
+    pedidos = db.query(PedidoModel).all()
+
+    return [
+        PedidoClienteOutput(
+            id=pedido.id,
+            status=pedido.status,
+            preco_total=pedido.preco_total
+        ) for pedido in pedidos
+    ]
+
+
+def get_detail_order(
+        db: Session,
+        order_id: int
+):
+    """
+    Retorna os detalhes de um pedido específico.
+
+    Args:
+        order_id (int): ID do pedido.
+        db (Session): Sessão do banco de dados.
+
+    Returns:
+        DetalhePedido: Detalhes do pedido.
+    """
+    # Busca o pedido pelo ID no banco de dados
+    pedido = db.query(PedidoModel).filter(PedidoModel.id == order_id).first()
+
+    # Verifica se o pedido foi encontrado
+    if not pedido:
+        return None
+
+    # Busca os itens associados ao pedido com join para ItemModel
+    itens_pedido = (
+        db.query(PedidoItensModel, ItemModel)
+        .join(ItemModel, PedidoItensModel.item_id == ItemModel.id)
+        .filter(PedidoItensModel.pedido_id == order_id)
+        .all()
+    )
+
+    # Extrai informações dos itens
+    itens_nomes = [item.ItemModel.nome for item in itens_pedido]
+    quantidades = [item.PedidoItensModel.quantidade for item in itens_pedido]
+    precos_unitarios = [item.ItemModel.preco for item in itens_pedido]
+
+    return DetalhePedido(
+        id=pedido.id,
+        itens=itens_nomes,
+        quantidade=quantidades,
+        precos_unitario=precos_unitarios,
+        preco_total=pedido.preco_total
+    )
+
+
+def get_all_categories(
+        db: Session
+):
+    """
+    Retorna todas as categorias disponíveis.
+
+    Args:
+        db (Session): Sessão do banco de dados.
+    Returns:
+        list: Lista de categorias.
+    """
+    # Busca todas as categorias no banco de dados
+    categorias = db.query(ItemModel.categoria).distinct().all()
+
+    # Extrai os nomes das categorias
+    return [categoria[0].upper() for categoria in categorias]
+
+
+def create_item(
+        db: Session,
+        nome: str,
+        descricao: str,
+        preco: float,
+        categoria: str,
+        arquivo: UploadFile = File(...)
+):
+    """
+    Cadastra um novo item no cardápio.
+
+    Args:
+        db (Session): Sessão do banco de dados.
+        nome (str): Nome do item.
+        descricao (str): Descrição do item.
+        preco (float): Preço do item.
+        categoria (str): Categoria do item.
+        arquivo (UploadFile): Imagem do item.
+    Returns:
+        MenuItem: Item cadastrado.
+    """
+    # Certifique-se de que o diretório de imagens existe
+    IMAGES_DIR.mkdir(parents=True, exist_ok=True)
+
+    # Obtém o maior ID existente ou 0 se a tabela estiver vazia
+    ultimo_id = db.query(func.max(ItemModel.id)).scalar() or 0
+
+    # Evita sobreposição de arquivos com o mesmo nome
+    arquivo.filename = f'{ultimo_id + 1}_{arquivo.filename}'
+
+    # Constroi o caminho completo do arquivo
+    caminho_arquivo = f'{IMAGES_DIR / arquivo.filename}'
+
+    # Salva o arquivo no diretório de imagens
+    with open(caminho_arquivo, "wb+") as objeto_arquivo:
+        objeto_arquivo.write(arquivo.file.read())
+
+    # Cria um novo item
+    novo_item = ItemModel(
+        nome=nome,
+        descricao=descricao,
+        preco=preco,
+        categoria=categoria,
+        url_imagem=caminho_arquivo
+    )
+
+    # Adiciona o item ao banco de dados
+    db.add(novo_item)
+    db.commit()
+    db.refresh(novo_item)
+
+    return novo_item
+
+
+def place_order(
+        db: Session,
+        pedido: PedidoClienteInput,
+        status: StatusPedido
+):
+    """
+    Processa um pedido do cliente.
+
+    Args:
+        pedido (PedidoClienteInput): Pedido do cliente.
+        status (StatusPedido): Status do pedido.
+        db (Session): Sessão do banco de dados.
+
+    Returns:
+        PedidoModel: Detalhes do pedido.
+
+    Raises:
+        HTTPException: Se algum item do pedido não for encontrado.
+    """
+    # Contar quantidades de cada item
+    itens_quantidades = Counter(pedido.itens)
+    preco_total = 0.0
+    itens_validados = []
+
+    # Validar todos os itens antes de criar o pedido
+    for item_id, quantidade in itens_quantidades.items():
+        item = get_item_by_id(db, item_id)
+
+        if not item:
+            return None
+
+        itens_validados.append((item, quantidade))
+        preco_total += item.preco * quantidade
+
+    # Criar novo pedido apenas após validação
+    novo_pedido = PedidoModel(
+        status=status.value,
+        preco_total=preco_total
+    )
+    db.add(novo_pedido)
+    db.commit()  # Salvar o pedido para gerar o ID
+
+    # Associar itens ao pedido
+    for item, quantidade in itens_validados:
+        pedido_itens = PedidoItensModel(
+            pedido_id=novo_pedido.id,
+            item_id=item.id,
+            quantidade=quantidade
+        )
+        db.add(pedido_itens)
+
+    db.commit()  # Salvar as associações
+    db.refresh(novo_pedido)
+
+    return novo_pedido
+
+
+def update_item(
+        db: Session,
+        item_id: int,
+        nome: str = None,
+        descricao: str = None,
+        preco: float = None,
+        categoria: str = None,
+        arquivo: UploadFile = File(None)
+):
+    """
+    Atualiza um item do cardápio.
+
+    Args:
+        db (Session): Sessão do banco de dados.
+        item_id (int): ID do item a ser atualizado.
+        nome (str): Novo nome do item.
+        descricao (str): Nova descrição do item.
+        preco (float): Novo preço do item.
+        categoria (str): Nova categoria do item.
+        arquivo (UploadFile): Nova imagem do item.
+    Returns:
+        MenuItem: Item atualizado.
+    """
+    # Busca o item pelo ID no banco de dados
+    item = db.query(ItemModel).filter(ItemModel.id == item_id).first()
+
+    # Verifica se o item foi encontrado
+    if not item:
+        return None
+
+    # Atualiza os campos do item
+    if nome:
+        item.nome = nome
+    if descricao:
+        item.descricao = descricao
+    if preco:
+        item.preco = preco
+    if categoria:
+        item.categoria = categoria
+
+    # Se um novo arquivo for fornecido, atualiza a imagem
+    if arquivo:
+        # Evita sobreposição de arquivos com o mesmo nome
+        arquivo.filename = f'{item_id}_{arquivo.filename}'
+
+        # Constroi o caminho completo do arquivo
+        caminho_arquivo = f'{IMAGES_DIR / arquivo.filename}'
+
+        with open(caminho_arquivo, "wb+") as objeto_arquivo:
+            objeto_arquivo.write(arquivo.file.read())
+
+        # Deleta o arquivo antigo se existir
+        if Path(item.url_imagem).exists():
+            Path(item.url_imagem).unlink()
+
+        # Atualiza a URL da imagem no banco de dados
+        item.url_imagem = caminho_arquivo
+
+    # Salva as alterações no banco de dados
+    db.commit()
+    db.refresh(item)
+
+    return MenuItem(**item.__dict__)
+
+
+def update_order_status(
+        db: Session,
+        order_id: int,
+        status: StatusPedido
+):
+    """
+    Atualiza o status de um pedido.
+
+    Args:
+        db (Session): Sessão do banco de dados.
+        order_id (int): ID do pedido a ser atualizado.
+        status (StatusPedido): Novo status do pedido.
+    Returns:
+        PedidoCliente: Pedido atualizado.
+    """
+    # Busca o pedido pelo ID no banco de dados
+    pedido = db.query(PedidoModel).filter(PedidoModel.id == order_id).first()
+
+    # Verifica se o pedido foi encontrado
+    if not pedido:
+        return None
+
+    # Atualiza o status do pedido
+    pedido.status = status.value
+
+    # Salva as alterações no banco de dados
+    db.commit()
+    db.refresh(pedido)
+
+    return pedido
+
+
+def update_order(
+        db: Session,
+        order_id: int,
+        pedido: PedidoClienteInput
+) -> PedidoClienteOutput:
+    """
+    Atualiza um pedido existente.
+
+    Args:
+        db (Session): Sessão do banco de dados.
+        order_id (int): ID do pedido a ser atualizado.
+        pedido (PedidoClienteInput): Novo pedido do cliente.
+
+    Returns:
+        PedidoClienteOutput: Detalhes do pedido atualizado.
+    """
+    # Busca o pedido pelo ID no banco de dados
+    pedido_db = db.query(PedidoModel).filter(
+        PedidoModel.id == order_id
+    ).first()
+
+    # Verifica se o pedido foi encontrado
+    if not pedido_db:
+        return None
+
+    # Busca todos os itens atuais associados ao pedido
+    itens_atuais = db.query(PedidoItensModel).filter(
+        PedidoItensModel.pedido_id == order_id
+    ).all()
+
+    # Lista de IDs de itens novos fornecidos na requisição
+    novos_itens_ids = [item for item in pedido.itens]
+
+    # Remove os itens que não estão mais na nova lista
+    for item_atual in itens_atuais:
+        if item_atual.item_id not in novos_itens_ids:
+            db.delete(item_atual)
+
+    # Verifica se o pedido está vazio (sem itens)
+    if not novos_itens_ids:
+        db.delete(pedido_db)  # Deleta o pedido do banco
+        db.commit()
+        return []  # Retorna [], pois o pedido foi removido
+
+    # Calcula o preço total e atualiza os itens
+    preco_total = 0.0
+    itens_contagem = {}  # Dicionário para contar a quantidade de cada item
+
+    # Conta a quantidade de cada item na lista de entrada
+    for item_id in pedido.itens:
+        itens_contagem[item_id] = itens_contagem.get(item_id, 0) + 1
+
+    # Atualiza ou adiciona os itens no pedido
+    for item_id, quantidade in itens_contagem.items():
+        item_db = db.query(ItemModel).filter(ItemModel.id == item_id).first()
+
+        if item_db:
+            # Verifica se o item já está no pedido
+            item_pedido_db = db.query(PedidoItensModel).filter(
+                PedidoItensModel.pedido_id == pedido_db.id,
+                PedidoItensModel.item_id == item_db.id
+            ).first()
+
+            if item_pedido_db:
+                # Atualiza a quantidade do item existente
+                item_pedido_db.quantidade = quantidade
+            else:
+                # Adiciona um novo item ao pedido
+                novo_item = PedidoItensModel(
+                    pedido_id=pedido_db.id,
+                    item_id=item_db.id,
+                    quantidade=quantidade
+                )
+                db.add(novo_item)
+
+            # Adiciona o preço do item ao preço total
+            preco_total += item_db.preco * quantidade
+
+    # Atualiza o preço total do pedido
+    pedido_db.preco_total = preco_total
+
+    # Salva as alterações no banco de dados
+    db.commit()
+    db.refresh(pedido_db)
+
+    return pedido_db
+
+
+def delete_item(
+        db: Session,
+        item_id: int
+):
+    """
+    Deleta um item do cardápio pelo ID.
+
+    Args:
+        item_id (int): ID do item.
+        db (Session): Sessão do banco de dados.
+    """
+    # Busca o item pelo ID no banco de dados
+    item = db.query(ItemModel).filter(ItemModel.id == item_id).first()
+
+    if not item:
+        return None
+
+    # Deleta o arquivo de imagem associado ao item
+    if item.url_imagem and Path(item.url_imagem).exists():
+        Path(item.url_imagem).unlink()
+
+    # Deleta o item do banco de dados
+    db.delete(item)
+    db.commit()
+
+    return db
+
+
+def delete_order(
+        db: Session,
+        order_id: int
+):
+    """
+    Deleta um pedido pelo ID.
+
+    Args:
+        order_id (int): ID do pedido.
+        db (Session): Sessão do banco de dados.
+    """
+    # Busca o pedido pelo ID no banco de dados
+    pedido = db.query(PedidoModel).filter(PedidoModel.id == order_id).first()
+
+    # Verifica se o pedido foi encontrado
+    if not pedido or (
+            pedido.status != StatusPedido.CANCELADO.value and
+            pedido.status != StatusPedido.ENTREGUE.value
+    ):
+        return None
+
+    # Deleta o pedido do banco de dados
+    db.delete(pedido)
+    db.commit()
+
+    return db
---
+++ src/menu/routers.py
@@ -0,0 +1,409 @@
+# Imports de terceiros
+from fastapi import APIRouter, File, UploadFile
+from fastapi.params import Depends
+from sqlalchemy.orm import Session
+
+
+# Imports locais
+from core.exceptions import APIException
+from core.schemas import SuccessResponse
+from src.menu.crud import (
+    create_item, delete_item, delete_order, get_all_categories,
+    get_all_orders, get_detail_order, get_item_by_id, get_menu,
+    place_order, update_item, update_order, update_order_status
+)
+from src.menu.schemas import PedidoClienteInput, StatusPedido
+from core.database import get_db
+
+router = APIRouter(
+    prefix="/cardapio",
+    tags=["cardapio"],
+    responses={404: {"description": "Not found"}},
+)
+
+
+@router.get("/obter_cardapio")
+async def obter_cardapio(
+        categoria: str = None,
+        db: Session = Depends(get_db)
+):
+    """
+    Retorna o cardápio completo ou filtrado por categoria do banco de dados.
+
+    Args:
+        categoria (str): Categoria para filtrar os itens do cardápio.
+        db (Session): Sessão do banco de dados.
+    Returns:
+        list: Lista de itens do cardápio.
+    """
+
+    cardapio = get_menu(db, categoria)
+
+    if len(cardapio) != 0:
+        return SuccessResponse(
+            data=cardapio,
+            message="Cardápio obtido com sucesso.",
+        )
+
+    raise APIException(
+        code=404,
+        description="Nenhum item encontrado.",
+        message="Nenhum item encontrado."
+    )
+
+
+@router.get("/obter_item/{item_id}")
+async def obter_item_id(
+        item_id: int,
+        db: Session = Depends(get_db)
+):
+    """
+    Retorna um item do cardápio pelo ID.
+
+    Args:
+        item_id (int): ID do item.
+        db (Session): Sessão do banco de dados.
+    Returns:
+        MenuItem: Item do cardápio.
+    """
+    # Busca o item pelo ID no banco de dados
+    item = get_item_by_id(db, item_id)
+
+    # Verifica se o item foi encontrado
+    if item:
+        return SuccessResponse(
+            data=item,
+            message="Item obtido com sucesso.",
+        )
+
+    raise APIException(
+        code=404,
+        description="Item não encontrado.",
+        message="Item não encontrado."
+    )
+
+
+@router.get("/obter_pedidos")
+async def obter_pedidos(
+        db: Session = Depends(get_db)
+):
+    """
+    Retorna todos os pedidos realizados.
+
+    Returns:
+        list: Lista de pedidos realizados.
+    """
+    pedidos = get_all_orders(db)
+
+    if len(pedidos) != 0:
+        return SuccessResponse(
+            data=pedidos,
+            message="Pedidos obtidos com sucesso.",
+        )
+
+    raise APIException(
+        code=404,
+        description="Nenhum pedido encontrado.",
+        message="Nenhum pedido encontrado."
+    )
+
+
+@router.get("/obter_detalhes_pedido/{pedido_id}")
+async def obter_detalhes_pedido(
+        pedido_id: int,
+        db: Session = Depends(get_db)
+):
+    """
+    Retorna os detalhes de um pedido específico.
+
+    Args:
+        pedido_id (int): ID do pedido.
+        db (Session): Sessão do banco de dados.
+
+    Returns:
+        PedidoCliente: Detalhes do pedido.
+    """
+    # Busca os detalhes do pedido no banco de dados
+    pedido_detalahdo = get_detail_order(db, pedido_id)
+
+    if pedido_detalahdo is not None:
+        return SuccessResponse(
+            data=pedido_detalahdo,
+            message="Pedido obtido com sucesso.",
+        )
+
+    raise APIException(
+        code=404,
+        description="Pedido não encontrado.",
+        message="Pedido não encontrado."
+    )
+
+
+@router.get("/obter_categorias")
+async def obter_categorias(
+        db: Session = Depends(get_db)
+):
+    """
+    Retorna todas as categorias do cardápio.
+
+    Args:
+        db (Session): Sessão do banco de dados.
+    Returns:
+        list: Lista de categorias.
+    """
+    # Busca as categorias no banco de dados
+    categorias = get_all_categories(db)
+
+    if len(categorias) != 0:
+        return SuccessResponse(
+            data=categorias,
+            message="Categorias obtidas com sucesso.",
+        )
+
+    raise APIException(
+        code=404,
+        description="Nenhuma categoria encontrada.",
+        message="Nenhuma categoria encontrada."
+    )
+
+
+@router.post("/cadastrar_item")
+async def cadastrar_item(
+        nome: str,
+        descricao: str,
+        preco: float,
+        categoria: str,
+        arquivo: UploadFile = File(...),
+        db: Session = Depends(get_db)
+):
+    """
+    Cadastra um novo item no cardápio.
+
+    Args:
+        nome (str): Nome do item.
+        descricao (str): Descrição do item.
+        preco (float): Preço do item.
+        categoria (str): Categoria do item.
+        arquivo (UploadFile): Imagem do item.
+        db (Session): Sessão do banco de dados.
+    Returns:
+        SuccessResponse: Mensagem de sucesso.
+    """
+    # Cria o item no banco de dados
+    item = create_item(db, nome, descricao, preco, categoria, arquivo)
+
+    if item:
+        return SuccessResponse(
+            data=None,
+            message="Item cadastrado com sucesso.",
+        )
+
+    raise APIException(
+        code=400,
+        description="Erro ao cadastrar o item.",
+        message="Erro ao cadastrar o item."
+    )
+
+
+@router.post("/fazer_pedido")
+async def fazer_pedido(
+        pedido: PedidoClienteInput,
+        status: StatusPedido,
+        db: Session = Depends(get_db)
+):
+    """
+    Realiza um pedido com os itens e quantidades especificadas.
+
+    Args:
+        pedido (PedidoRequest): Detalhes do pedido, incluindo
+        itens e quantidades.
+        status (str): Status do pedido.
+        db (Session): Sessão do banco de dados.
+
+    Returns:
+        SuccessResponse: Confirmação do pedido.
+    """
+
+    pedido_cliente = place_order(db, pedido, status)
+
+    if pedido_cliente:
+        return SuccessResponse(
+            data=None,
+            message="Pedido realizado com sucesso."
+        )
+
+    raise APIException(
+        code=400,
+        description="O pedido não pode ser realizado.",
+        message="O pedido não pode ser realizado."
+    )
+
+
+@router.put("/atualizar_item/{item_id}")
+async def atualizar_item(
+        item_id: int,
+        nome: str = None,
+        descricao: str = None,
+        preco: float = None,
+        categoria: str = None,
+        arquivo: UploadFile = File(None),
+        db: Session = Depends(get_db)
+):
+    """
+    Atualiza um item do cardápio.
+
+    Args:
+        item_id (int): ID do item a ser atualizado.
+        nome (str): Novo nome do item.
+        descricao (str): Nova descrição do item.
+        preco (float): Novo preço do item.
+        categoria (str): Nova categoria do item.
+        arquivo (UploadFile): Nova imagem do item.
+        db (Session): Sessão do banco de dados.
+    Returns:
+        SuccessResponse: Mensagem de sucesso.
+    """
+    # Atualiza o item no banco de dados
+    item = update_item(db, item_id, nome, descricao, preco, categoria, arquivo)
+
+    if item:
+        return SuccessResponse(
+            data=None,
+            message="Item atualizado com sucesso.",
+        )
+
+    raise APIException(
+        code=404,
+        description="Item não encontrado.",
+        message="Item não encontrado."
+    )
+
+
+@router.put("/atualizar_status_pedido/{pedido_id}")
+async def atualizar_status_pedido(
+        pedido_id: int,
+        status: StatusPedido,
+        db: Session = Depends(get_db)
+):
+    """
+    Atualiza o status de um pedido.
+
+    Args:
+        pedido_id (int): ID do pedido a ser atualizado.
+        status (str): Novo status do pedido.
+        db (Session): Sessão do banco de dados.
+    Returns:
+        SuccessResponse: Mensagem de sucesso.
+    """
+    # Atualiza o status do pedido no banco de dados
+    pedido = update_order_status(db, pedido_id, status)
+
+    if pedido:
+        return SuccessResponse(
+            data=None,
+            message="Status do pedido atualizado com sucesso.",
+        )
+
+    raise APIException(
+        code=404,
+        description="Pedido não encontrado.",
+        message="Pedido não encontrado."
+    )
+
+
+@router.put("/atualizar_pedido/{pedido_id}")
+async def atualizar_pedido(
+        pedido_id: int,
+        pedido: PedidoClienteInput,
+        db: Session = Depends(get_db)
+):
+    """
+    Atualiza um pedido existente.
+
+    Args:
+        pedido_id (int): ID do pedido a ser atualizado.
+        pedido (PedidoRequest): Detalhes do pedido, incluindo
+        itens e quantidades.
+        db (Session): Sessão do banco de dados.
+    Returns:
+        SuccessResponse: Mensagem de sucesso.
+    """
+    # Atualiza o pedido no banco de dados
+    pedido_cliente = update_order(db, pedido_id, pedido)
+
+    try:
+        if pedido_cliente:
+            return SuccessResponse(
+                data=None,
+                message="Pedido atualizado com sucesso."
+            )
+        elif len(pedido_cliente) == 0:
+            return SuccessResponse(
+                data=None,
+                message="Pedido deletado com sucesso devido está vazio."
+            )
+    except TypeError:
+        raise APIException(
+            code=404,
+            description="O pedido não foi encontrado.",
+            message="O pedido não foi encontrado."
+        )
+
+
+@router.delete("/deletar_item/{item_id}")
+async def deletar_item(item_id: int, db: Session = Depends(get_db)):
+    """
+    Deleta um item do cardápio.
+
+    Args:
+        item_id (int): ID do item a ser deletado.
+        db (Session): Sessão do banco de dados.
+    Returns:
+        SuccessResponse: Mensagem de sucesso.
+    """
+    # Verifica se o item existe no banco de dados
+    item = delete_item(db, item_id)
+
+    # Se o item existir, deleta-o
+    if item:
+        return SuccessResponse(
+            data=None,
+            message="Item deletado com sucesso.",
+        )
+
+    raise APIException(
+        code=404,
+        description="Item não encontrado.",
+        message="Item não encontrado."
+    )
+
+
+@router.delete("/deletar_pedido/{pedido_id}")
+async def deletar_pedido(
+        pedido_id: int,
+        db: Session = Depends(get_db)
+):
+    """
+    Deleta um pedido.
+
+    Args:
+        pedido_id (int): ID do pedido a ser deletado.
+        db (Session): Sessão do banco de dados.
+    Returns:
+        SuccessResponse: Mensagem de sucesso.
+    """
+    # Verifica se o pedido existe no banco de dados
+    pedido = delete_order(db, pedido_id)
+
+    # Se o pedido existir, deleta-o
+    if pedido:
+        return SuccessResponse(
+            data=None,
+            message="Pedido deletado com sucesso.",
+        )
+
+    raise APIException(
+        code=404,
+        description="Pedido não encontrado ou não pode ser deletado.",
+        message="Pedido não encontrado ou não pode ser deletado."
+    )
