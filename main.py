e2b16f466b3423490f34f84daf153a7f2036ff09
Welison92 <welison123santos91@gmail.com>
2025-05-24 14:57:30 -0300

    Atualização do Dockerfile e main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 # Imports de terceiros
 from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
-from pathlib import Path
 from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import JSONResponse
 
@@ -14,12 +13,6 @@ app = FastAPI(
     title="CardapioVirtual_API",
     version="0.0.1"
 )
-
-BASE_DIR = Path(__file__).resolve().parent.parent.parent  # Raiz do projeto
-IMAGES_DIR = BASE_DIR / "static" / "images"  # Diretório das imagens
-
-# Certifique-se de que o diretório de imagens existe
-IMAGES_DIR.mkdir(parents=True, exist_ok=True)
 
 # Monta a pasta 'static' para servir arquivos estáticos
 app.mount("/static", StaticFiles(directory="static"), name="static")
